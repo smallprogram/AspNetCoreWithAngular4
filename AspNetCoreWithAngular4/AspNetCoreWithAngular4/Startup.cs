73a73031cec0e497f501d3af7f379bdcd09a8797
朱先生 <smallprogram@foxmail.com>
2017-07-07 00:56:04 +0800

    Update

--- AspNetCoreWithAngular4/AspNetCoreWithAngular4/Startup.cs
+++ AspNetCoreWithAngular4/AspNetCoreWithAngular4/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+
 namespace AspNetCoreWithAngular4
 {
     public class Startup
@@ -16,6 +17,7 @@ namespace AspNetCoreWithAngular4
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +30,21 @@ namespace AspNetCoreWithAngular4
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.Use(async (context, next) => {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                   !System.IO.Path.HasExtension(context.Request.Path.Value) &&
+                   !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
             });
+
+            app.UseMvcWithDefaultRoute();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
